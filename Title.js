d96c26e551a0ff00ca8e90a63ccfa18dcc885659
CJ Quines <cj@cjquines.com>
2021-12-25 23:06:34 -0500

    styling

--- Title.js
+++ Title.js
@@ -1,6 +1,6 @@
 import { html, render, React, css } from "./package.js";
 
-import { Cards } from "./Cards.js";
+import { Toggle } from "./Toggle.js";
 
 const Title = () => {
   const [hunt, setHunt] = React.useState(true);
@@ -52,77 +52,15 @@ const Title = () => {
     right: 0;
   `;
 
-  const button = css`
-    background: none;
-    border: none;
-    box-sizing: border-box;
-    color: inherit;
-    display: block;
-    font-size: 0;
-    line-height: inherit;
-    padding: 0.4rem 0 0.4rem 4rem;
-    position: absolute;
-    text-align: left;
-
-    &[aria-pressed]::after {
-      border-radius: 50%;
-      background: #87782c;
-      box-shadow: 0 0 0.5rem 0.5rem #87782c;
-      content: "";
-      height: 1rem;
-      left: 0.65rem;
-      position: absolute;
-      top: 0.65rem;
-      transition: all 0.25s ease;
-      width: 1rem;
-    }
-
-    &[aria-pressed="true"]::after {
-      background: #798a91;
-      box-shadow: 0 0 0.5rem 0.5rem #798a91;
-      left: 2.8rem;
-    }
-  `;
-
   const toggle = (e) => {
     e.stopPropagation();
     setHunt(!hunt);
   };
 
-  const cards = css`
-    border-radius: 1.15rem;
-    position: relative;
-    height: 2.3rem;
-    width: 4.5rem;
-    top: -0.5rem;
-    background: #39170d;
-    box-shadow: inset 0 1px 2px 4px #1f0c0a47;
-
-    img {
-      height: 1.5rem;
-      padding: 0.4rem;
-      position: absolute;
-      width: 1.5rem;
-      z-index: 1;
-    }
-  `;
-
   return html`
     <div class=${title} onClick=${toggle}>
       <h1 class="${headerH1} ${header1}">Mystery <br />Hunt</h1>
-      <div class=${cards}>
-        <button
-          class=${button}
-          type="button"
-          aria-pressed=${!hunt}
-          onClick=${toggle}
-        >
-          Toggle content between Mystery Hunt and Puzzle Club
-        </button>
-        <img src="images/coin.png" />
-        <img style="right:0.15rem" src="images/tools.png" />
-      </div>
-      <!-- <${Cards} hunt=${hunt} interactive="true" /> -->
+      <${Toggle} hunt=${hunt} toggle=${toggle} />
       <h1 class="${headerH1} ${header2}">Puzzle <br />Club</h1>
     </div>
   `;
---
+++ Toggle.js
@@ -0,0 +1,94 @@
+import { html, render, React, css } from "./package.js";
+
+/**
+ * hunt: true if Hunt side should be shown, false if Club side
+ * toggle: function for toggling hunt. if falsy, show non-interactive version
+ */
+const Toggle = ({ hunt, toggle }) => {
+  const toggleCss = css`
+    background: #39170d;
+    border-radius: 1.15rem;
+    box-shadow: inset 0 0.1rem 0.2rem 0.4rem #1f0c0a47;
+    height: 2.3rem;
+    position: relative;
+    top: -0.5rem;
+    transition: 0.2s box-shadow;
+    width: 4.5rem;
+
+    div:hover & {
+      box-shadow: inset 0 0.1rem 0.2rem 0.4rem #1f0c0a47, 0 0 0.5rem #b5815c;
+    }
+
+    @media (max-width: 600px) {
+      top: 0;
+    }
+  `;
+
+  const button = css`
+    background: none;
+    border: none;
+    display: block;
+    font-size: 0;
+    position: absolute;
+
+    &[aria-pressed]::after {
+      border-radius: 50%;
+      background: #87782c;
+      box-shadow: 0 0 0.4rem 0.5rem #87782c;
+      content: "";
+      height: 1rem;
+      left: 0.65rem;
+      position: absolute;
+      top: 0.65rem;
+      transition: background 0.3s, box-shadow 0.3s, left 0.3s;
+      width: 1rem;
+    }
+
+    &[aria-pressed="true"]::after {
+      background: #798a91;
+      box-shadow: 0 0 0.4rem 0.5rem #798a91;
+      left: 2.8rem;
+    }
+  `;
+
+  const img = css`
+    height: 1.5rem;
+    padding: 0.4rem;
+    position: absolute;
+    transition: 0.3s opacity;
+    width: 1.5rem;
+    z-index: 1;
+  `;
+
+  const img1 = css`
+    opacity: 1;
+
+    button[aria-pressed="true"] ~ & {
+      opacity: 0.3;
+    }
+  `;
+
+  const img2 = css`
+    opacity: 0.3;
+    right: 0.15rem;
+
+    button[aria-pressed="true"] ~ & {
+      opacity: 1;
+    }
+  `;
+
+  return html`<div class=${toggleCss}>
+    <button
+      class=${button}
+      type="button"
+      aria-pressed=${!hunt}
+      onClick=${toggle}
+    >
+      Toggle content between Mystery Hunt and Puzzle Club
+    </button>
+    <img class="${img} ${img1}" src="images/coin.png" />
+    <img class="${img} ${img2}" src="images/tools.png" />
+  </div>`;
+};
+
+export { Toggle };
